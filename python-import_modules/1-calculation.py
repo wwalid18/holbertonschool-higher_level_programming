42dd25b653d0ac8bd8b050efd89f37d785315ab4
wwalid18 <agar.io.annonyms@gmail.com>
2025-01-19 22:48:11 +0100

    task 1

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -4,4 +4,9 @@ from calculator_1 import add, sub, mul, div
 if __name__ == "__main__":
     a = 10
     b = 5
-    print(f"{a} + {b} = {add(a, b)}\n{a} - {b} = {sub(a, b)}\n{a} * {b} = {mul(a, b)}\n{a} / {b} = {div(a, b)}")
+print(f"""
+{a} + {b} = {add(a, b)}
+{a} - {b} = {sub(a, b)}
+{a} * {b} = {mul(a, b)}
+{a} / {b} = {div(a, b)}
+""")
