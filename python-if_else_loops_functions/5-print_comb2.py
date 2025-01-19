2713b5e8df9496dee056234e03570432047e215a
wwalid18 <agar.io.annonyms@gmail.com>
2025-01-19 10:44:37 +0100

    task 5

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 for i in range(0, 100):
-    if i != 99:
-        print("{:02d}".format(i), end=", ")
-    else:
-        print("{:02d}".format(i))
+    print("{:02d}".format(i), end=", " if i < 98 else "\n")
