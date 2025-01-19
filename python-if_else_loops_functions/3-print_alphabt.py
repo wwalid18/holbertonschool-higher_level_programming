e1d49a048fd1b54ebfd9b775a35933f95d16f605
wwalid18 <agar.io.annonyms@gmail.com>
2025-01-19 10:22:32 +0100

    task 3

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,2 +1,3 @@
 #!/usr/bin/python3
-print("".join("{:c}".format(i) for i in range(97, 123) if i not in (101, 113)), end="")
+print("".join("{:c}".format(i) for i in range(97, 123)
+              if i not in (101, 113)), end="")
