e1739e3cffd099641797ac08198c54286d4bee11
wwalid18 <agar.io.annonyms@gmail.com>
2025-01-19 11:53:40 +0100

    task 0

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-import add_0
+from add_0 import add
 
 if __name__ == "__main__":
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, add_0.add(a, b)))
+    print(f"{a} + {b} = {add(a, b)}")
