0446ca0cc3048da05a10fb9fa0f77f57c202ee5e
wwalid18 <agar.io.annonyms@gmail.com>
2025-01-21 11:30:37 +0100

    this weeks preparation

---
+++ python-exceptions/README.md
@@ -0,0 +1,1 @@
+non empty readmee fo shooo
---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,19 @@
+def add_integer(a, b=98):
+    """
+    Adds two integers or floats after casting them to integers.
+
+    Args:
+        a: The first number, must be an integer or float.
+        b: The second number, must be an integer or float (default is 98).
+
+    Returns:
+        The integer addition of a and b.
+
+    Raises:
+        TypeError: If a or b is not an integer or float.
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
