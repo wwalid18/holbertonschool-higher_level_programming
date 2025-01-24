90be484e073bbc1f4042298833c9c34a1d52d269
wwalid18 <agar.io.annonyms@gmail.com>
2025-01-24 13:46:10 +0100

    task 0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -36,6 +36,10 @@ def add_integer(a, b=98):
             ...
         TypeError: a must be an integer
     """
+    if isinstance(a, float) and a != a:
+        raise TypeError("a must be an integer")
+    if isinstance(b, float) and b != b:
+        raise TypeError("b must be an integer")
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
