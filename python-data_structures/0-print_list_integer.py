ccb6d6648d0c738603ba83e789b4356973873276
wwalid18 <agar.io.annonyms@gmail.com>
2025-01-24 12:48:26 +0100

    task 0

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    """
-    Prints all integers of a list, one integer per line using str.format().
-    """
     for number in my_list:
         print("{:d}".format(number))
