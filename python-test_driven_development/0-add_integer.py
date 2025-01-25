9a5b04bf6f2ac47cd8ac0b5ffe536d0b874fc60a
wwalid18 <agar.io.annonyms@gmail.com>
2025-01-25 13:01:59 +0100

    task 0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,4 +1,26 @@
 #!/usr/bin/python3
+"""
+This module provides a function to add two integers.
+
+Examples:
+    >>> add_integer(1, 2)
+    3
+    >>> add_integer(100, -2)
+    98
+    >>> add_integer(2)
+    100
+    >>> add_integer(100.3, -2)
+    98
+    >>> add_integer(4, "School")
+    Traceback (most recent call last):
+        ...
+    TypeError: b must be an integer
+    >>> add_integer(None)
+    Traceback (most recent call last):
+        ...
+    TypeError: a must be an integer
+"""
+
 
 def add_integer(a, b=98):
     """
@@ -18,5 +40,4 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     if type(b) not in [int, float]:
         raise TypeError("b must be an integer")
-    
     return round(a) + round(b)
