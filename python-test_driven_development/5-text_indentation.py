967db59c327a0e1f7291fd52fd2f3591ff64b5c1
wwalid18 <agar.io.annonyms@gmail.com>
2025-01-25 14:57:36 +0100

    task 5

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,32 +1,23 @@
 #!/usr/bin/python3
 """
 This module provides a function that prints a text without indentation
-
 Examples:
 >>> text_indentation = __import__('5-text_indentation').text_indentation
->>> text_indentation("Lorem ipsum dolor sit amet, consectetur
-adipiscing elit. Quonam modo? Utrum igitur tibi litteram videor
-an totas paginas commovere? Non autem hoc: igitur ne illud quidem.")
+>>> text_indentation("Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+Quonam modo? Utrum igitur tibi litteram videor an totas paginas commovere?
+Non autem hoc: igitur ne illud quidem.")
 Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-
 Quonam modo?
-
 Utrum igitur tibi litteram videor an totas paginas commovere?
-
 Non autem hoc:
-
 igitur ne illud quidem.
 >>> text_indentation("This is a test. It works well. Does it? Yes.")
 This is a test.
-
 It works well.
-
 Does it?
-
 Yes.
 >>> text_indentation("Hello world: this is a test.")
 Hello world:
-
 this is a test.
 >>> text_indentation(123)
 Traceback (most recent call last):
@@ -50,12 +41,13 @@ def text_indentation(text):
     """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
+    if text == "":
+        raise TypeError("text must be a string")
     i = 0
     length = len(text)
     while i < length:
         print(text[i], end="")
         if text[i] in '.?:':
-            print("\n")
             while i + 1 < length and text[i + 1] == " ":
                 i += 1
         i += 1
