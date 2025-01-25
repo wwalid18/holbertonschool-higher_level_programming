ddac3c560f50c8b327fbbde551bd488d1253d4ca
wwalid18 <agar.io.annonyms@gmail.com>
2025-01-25 13:24:33 +0100

    task 2

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -39,17 +39,17 @@ def matrix_divided(matrix, div):
     """
     if not isinstance(matrix, list) or \
        not all(isinstance(row, list) for row in matrix):
-        raise TypeError("matrix must be a matrix (list of lists) \
+        raise TypeError("matrix must be a matrix (list of lists)\
                          of integers/floats")
     if not all(isinstance(item, (int, float))
                for row in matrix for item in row):
-        raise TypeError("matrix must be a matrix (list of lists) \
+        raise TypeError("matrix must be a matrix (list of lists)\
                          of integers/floats")
 
     row_size = len(matrix[0])
 
     if not all(len(row) == row_size for row in matrix):
-        raise TypeError("Each row of the matrix  \
+        raise TypeError("Each row of the matrix \
                         must have the same size")
 
     if not isinstance(div, (int, float)):
