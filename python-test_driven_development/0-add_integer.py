e42204014c8fc6047db89dde3c43eb8e1cec83a6
wwalid18 <agar.io.annonyms@gmail.com>
2025-01-25 12:57:54 +0100

    task 0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -2,21 +2,24 @@
 
 def add_integer(a, b=98):
     """
-    Adds two integers or floats, rounding them to the nearest integer.
+    Adds two integers or floats and returns their sum.
 
-    Args:
-    a (int, float): The first integer or float.
-    b (int, float): The second integer or float. Default is 98.
+    The function checks if both `a` and `b` are either integers or floats. 
+    It raises a TypeError if either `a` or `b` is not an integer or float.
+
+    Parameters:
+    - a: The first number (integer or float).
+    - b: The second number (integer or float). Defaults to 98 if not provided.
 
     Returns:
-    int: The sum of a and b after rounding them.
+    - The sum of `a` and `b` as an integer, rounding any floats to the nearest integer.
 
     Raises:
-    TypeError: If a or b is not an integer or float.
+    - TypeError: If either `a` or `b` is not an integer or a float.
     """
     if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
     if type(b) not in [int, float]:
         raise TypeError("b must be an integer")
     
-    return round(a) + round(b)
+    return int(a) + int(b)
