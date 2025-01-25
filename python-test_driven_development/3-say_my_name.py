d199a9ac3b009deb6ce766b53fedd6f6a9b1e91e
wwalid18 <agar.io.annonyms@gmail.com>
2025-01-25 14:06:41 +0100

    task 3

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -22,18 +22,18 @@ TypeError: last_name must be a string
 
 def say_my_name(first_name, last_name=""):
     """
-    Prints the name in the format "My name is <first name> <last name>".
-
+    Prints the name in the format "My name is <first_name> <last_name>"
+    
     Args:
-    first_name (str): The first name.
-    last_name (str, optional): The last name. Defaults to an empty string.
-
+    first_name (str): The first name
+    last_name (str, optional): The last name (default is "")
+    
     Raises:
-    TypeError: If either first_name or last_name is not a string.
+    TypeError: If either first_name or last_name is not a string
     """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
 
-    print(f"My name is {first_name} {last_name}")
+    print(f"My name is {first_name} {last_name}".strip())
