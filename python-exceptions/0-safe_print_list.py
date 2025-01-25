0c622547c78cf41f7104790edbea7257928267d6
wwalid18 <agar.io.annonyms@gmail.com>
2025-01-25 15:34:47 +0100

    task 0

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,10 @@
+def safe_print_list(my_list=[], x=0):
+    printed = 0
+    try:
+        for i in range(x):
+            print(my_list[i], end="")
+            printed += 1
+    except IndexError:
+        pass
+    print()
+    return printed
