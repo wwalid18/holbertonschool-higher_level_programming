3f9938b0c7d1183fb135e9724c5b20f292364532
wwalid18 <agar.io.annonyms@gmail.com>
2025-01-25 14:11:28 +0100

    task 3

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -36,4 +36,4 @@ def say_my_name(first_name, last_name=""):
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
     
-    print(f"My name is {first_name} {last_name}".strip())
+    print(f"My name is {first_name} {last_name} ".strip())
