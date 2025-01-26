54677235ca9481e8cff433a6b1427a44e80bc96a
wwalid18 <agar.io.annonyms@gmail.com>
2025-01-26 23:12:07 +0100

    task 0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,10 +1,11 @@
+#!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    printed = 0
+    count = 0
     try:
         for i in range(x):
             print(my_list[i], end="")
-            printed += 1
+            count += 1
     except IndexError:
         pass
     print()
-    return printed
+    return count
