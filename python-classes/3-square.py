cd142ace823ffe810d1d1ff15242b0f72491a69a
wwalid18 <agar.io.annonyms@gmail.com>
2025-01-28 13:19:35 +0100

    task 3

---
+++ python-classes/3-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+This module defines the Square class.
+"""
+
+
+class Square:
+    """a square class
+    """
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    def area(self):
+        return self.__size ** 2
