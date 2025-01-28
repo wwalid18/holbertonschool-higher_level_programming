0eaf63778b60e989d13fb86397967d11226a78eb
wwalid18 <agar.io.annonyms@gmail.com>
2025-01-28 12:56:09 +0100

    task 0 and 1

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
+"""Defines an empty class Square.
+"""
+
+
 class Square:
+    """a square class
+    """
     pass
--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -5,5 +5,7 @@ This module defines the Square class.
 
 
 class Square:
+    """a square class
+    """
     def __init__(self, size):
         self.__size = size
