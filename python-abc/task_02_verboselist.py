b411c7d7567345c6bf9d3cfda9c382c9a5f6d78d
wwalid18 <agar.io.annonyms@gmail.com>
2025-02-09 16:38:27 +0100

    task 2

---
+++ python-abc/task_02_verboselist.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+class VerboseList(list):
+    """A list that prints notifications when items are added or removed"""
+
+    def append(self, item):
+        super().append(item)
+        print(f"Added [{item}] to the list.")
+
+    def extend(self, items):
+        super().extend(items)
+        print(f"Extended the list with [{len(items)}] items.")
+
+    def remove(self, item):
+        print(f"Removed [{item}] from the list.")
+        super().remove(item)
+
+    def pop(self, index=-1):
+        item = super().pop(index)
+        print(f"Popped [{item}] from the list.")
+        return item
