820f42e054cdb8eecde43ae27ccfc6ef23b86855
wwalid18 <agar.io.annonyms@gmail.com>
2025-02-09 12:51:36 +0100

    using the math modulee

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 from abc import ABC, abstractmethod
+import math
 
 
 class Shape(ABC):
@@ -19,10 +20,10 @@ class Circle(Shape):
         self.radius = radius
 
     def area(self):
-        return 3.14 * self.radius ** 2
+        return math.pi * self.radius ** 2
 
     def perimeter(self):
-        return 2 * 3.14 * self.radius
+        return 2 * math.pi * self.radius
 
 
 class Rectangle(Shape):
