f9e3c28ffc6f32a37a70e7170c6462754bd8a89a
wwalid18 <agar.io.annonyms@gmail.com>
2025-02-09 16:54:09 +0100

    task 1

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -12,33 +12,3 @@ class Shape(ABC):
     @abstractmethod
     def perimeter(self):
         pass
-
-class Circle(Shape):
-    """Concrete class representing a Circle."""
-
-    def __init__(self, radius):
-        self.radius = radius
-
-    def area(self):
-        return math.pi * (self.radius ** 2)
-
-    def perimeter(self):
-        return 2 * math.pi * self.radius
-
-class Rectangle(Shape):
-    """Concrete class representing a Rectangle."""
-    
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
-    
-    def area(self):
-        return self.width * self.height
-    
-    def perimeter(self):
-        return 2 * (self.width + self.height)
-
-def shape_info(shape):
-    """Prints the area and perimeter of a shape, relying on duck typing."""
-    print(f"Area: {shape.area()}")
-    print(f"Perimeter: {shape.perimeter()}")
