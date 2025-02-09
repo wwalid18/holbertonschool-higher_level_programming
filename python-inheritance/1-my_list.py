6c56ee363f92b458c2ffb15878dd8e145d1cd6ed
wwalid18 <agar.io.annonyms@gmail.com>
2025-02-09 16:07:14 +0100

    task 1

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -4,7 +4,7 @@
 
 class MyList(list):
     """A subclass of list that includes a method to print a sorted version."""
-    
+
     def print_sorted(self):
         """Prints the list in ascending sorted order."""
         print(sorted(self))
