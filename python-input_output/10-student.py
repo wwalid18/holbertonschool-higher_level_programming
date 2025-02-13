18cc3d42e734a1b2e03f2fa4aa52d7975939dba2
wwalid18 <agar.io.annonyms@gmail.com>
2025-02-13 15:26:48 +0100

    task 10

---
+++ python-input_output/10-student.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""class student"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        return {
+            key: value
+            for key, value in self.__dict__.items()
+            if key in attrs
+        }
