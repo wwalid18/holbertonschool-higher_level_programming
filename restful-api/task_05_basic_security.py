50c40b2c8a728fc99c90b4528114d853f3cad35c
wwalid18 <agar.io.annonyms@gmail.com>
2025-02-23 13:32:21 +0100

    task 5

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -13,7 +13,18 @@ auth = HTTPBasicAuth()
 jwt = JWTManager(app)
 app.config['SECRET_KEY'] = 'your_secret_key'
 
-users = {}
+users = {
+    "user1": {
+        "username": "user1",
+        "password": generate_password_hash("password"),
+        "role": "user"
+    },
+    "admin1": {
+        "username": "admin1",
+        "password": generate_password_hash("password"),
+        "role": "admin"
+    }
+}
 
 
 @auth.verify_password
