0788923b3a5289883b00f778c0c892e4b0c34939
wwalid18 <agar.io.annonyms@gmail.com>
2025-03-10 16:41:39 +0100

    task 6

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-"""
-file that contains the class definition of a State
-and an instance Base = declarative_base()
-"""
+"""Define the State class and connect to the database"""
 
 from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
