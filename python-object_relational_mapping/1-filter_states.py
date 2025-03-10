89e6c9a3b1f4342d0b95901ac41e58a8e274b188
wwalid18 <agar.io.annonyms@gmail.com>
2025-03-10 15:08:57 +0100

    task 1

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Lists all states that start with N from the database hbtn_0e_0_usa"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    username, password, db_name = sys.argv[1], sys.argv[2], sys.argv[3]
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=db_name,
+        charset="utf8"
+    )
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE NAME LIKE BINARY 'N%' ORDER BY id ASC")
+    for row in cur.fetchall():
+        print(row)
+    cur.close()
+    db.close()
