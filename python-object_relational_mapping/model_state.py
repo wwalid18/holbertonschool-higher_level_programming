d602a847fcf12e57a38a553563b4bad034f9aa98
wwalid18 <agar.io.annonyms@gmail.com>
2025-03-10 16:38:53 +0100

    task 6

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,14 +1,36 @@
 #!/usr/bin/python3
-"""Define the State class and connect to the database"""
-from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
+"""
+file that contains the class definition of a State
+and an instance Base = declarative_base()
+"""
 
-# Create the base class
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+import sys
+
 Base = declarative_base()
 
-# Define the State class
-class State(Base):
-    __tablename__ = 'states'
 
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+class State(Base):
+    """
+    State class that defines the structure of states table
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+
     name = Column(String(128), nullable=False)
+
+
+if __name__ == "__main__":
+    # Check input arguments
+    if len(sys.argv) != 4:
+        print(f"Usage: {sys.argv[0]} "
+              "<username> <password> <database>")
+        sys.exit(1)
+
+    # Create an engine that connects to the MySQL server
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+
+    # Create all tables in the engine
+    Base.metadata.create_all(engine)
