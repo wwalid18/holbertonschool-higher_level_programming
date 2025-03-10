dcfc82a746915aa683468d3fdfa134d9884c2aed
wwalid18 <agar.io.annonyms@gmail.com>
2025-03-10 16:08:02 +0100

    task 5

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+Lists all states where the name matches in the database hbtn_0e_0_usa
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=db_name,
+        charset="utf8"
+    )
+
+    cur = db.cursor()
+    query = """
+    SELECT cities.name
+    FROM cities
+    JOIN states ON cities.state_id = states.id
+    WHERE states.name = %s
+    ORDER BY cities.id ASC
+    """
+    cur.execute(query, (state_name,))
+
+    cities = [row[0] for row in cur.fetchall()]
+    print(", ".join(cities))
+
+    cur.close()
+    db.close()
