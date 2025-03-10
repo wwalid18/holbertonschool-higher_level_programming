e9a90e2624cdf29d9c1cd5df4c9f948d87d93696
wwalid18 <agar.io.annonyms@gmail.com>
2025-03-10 16:43:47 +0100

    task 7

---
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+lists all State objects
+"""
+
+from sqlalchemy import (create_engine)
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+
+    if len(sys.argv) != 4:
+        print(f"Usage: {sys.argv[0]} "
+              "<mysql username> <mysql password> <database name>")
+        sys.exit(1)
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for state in session.query(State).order_by(State.id):
+        print(f"{state.id}: {state.name}")
+    session.close()
