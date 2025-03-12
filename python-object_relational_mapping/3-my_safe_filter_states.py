e00ae418717fc82404ee09b16f3295755d399b2d
wwalid18 <agar.io.annonyms@gmail.com>
2025-03-12 11:46:24 +0100

    task 3

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -11,20 +11,20 @@ if __name__ == "__main__":
 
     username = sys.argv[1]
     password = sys.argv[2]
-    db_name = sys.argv[3]
+    database = sys.argv[3]
     state_name = sys.argv[4]
 
     db = MySQLdb.connect(
         host="localhost",
-        port=3306,
         user=username,
         passwd=password,
-        db=db_name,
-        charset="utf8"
+        db=database,
+        port=3306
     )
 
     cur = db.cursor()
     query = "SELECT * FROM states WHERE name LIKE BINARY %s ORDER BY id ASC"
+
     cur.execute(query, (state_name,))
 
     for row in cur.fetchall():
