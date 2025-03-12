b9c5fbf13ceae262900474e6803be696f0ad8bbd
wwalid18 <agar.io.annonyms@gmail.com>
2025-03-12 11:43:10 +0100

    task 2

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,25 +1,29 @@
 #!/usr/bin/python3
 """
-Lists all states where the name matches in the database hbtn_0e_0_usa
+Lists all states where the name matches
+in the database hbtn_0e_0_usa
 """
 
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    
+    """
+    Connects to MySQL and retrieves states where
+    name matches user input.
+    """
+
     username = sys.argv[1]
     password = sys.argv[2]
-    db_name = sys.argv[3]
+    database = sys.argv[3]
     state_name = sys.argv[4]
 
     db = MySQLdb.connect(
         host="localhost",
-        port=3306,
         user=username,
         passwd=password,
-        db=db_name,
-        charset="utf8"
+        db=database,
+        port=3306
     )
 
     cur = db.cursor()
