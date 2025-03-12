b04a5335620bf4d1328a2aa9ed8f274ccc2ded8a
wwalid18 <agar.io.annonyms@gmail.com>
2025-03-12 11:49:02 +0100

    task 3

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Listing states from a db
+Lists all states where the name matches in the database hbtn_0e_0_usa
 """
 
 import MySQLdb
@@ -8,36 +8,26 @@ import sys
 
 if __name__ == "__main__":
 
-    # Check input arguments
-    if len(sys.argv) != 5:
-        print(f"Usage: {sys.argv[0]} "
-              "<username> <password> <database> <state name>")
-        sys.exit(1)
-
-    # Catch db credentials
-    MY_HOST = "localhost"
-    MY_USER = sys.argv[1]
-    MY_PASS = sys.argv[2]
-    MY_DB = sys.argv[3]
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
     state_name = sys.argv[4]
 
-    # Connection to DB
-    db = MySQLdb.connect(host=MY_HOST,
-                         user=MY_USER,
-                         passwd=MY_PASS,
-                         db=MY_DB,
-                         port=3306
-                         )
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=db_name,
+        charset="utf8"
+    )
 
-    # Cursor creation to execute SQL queries
-    cursor = db.cursor()
+    cur = db.cursor()
+    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+    cur.execute(query, (state_name,))
 
-    # Print results in comma delimited format
-    cursor.execute("SELECT * FROM states WHERE name = %s", (state_name,))
-    rows = cursor.fetchall()
-    for row in rows:
+    for row in cur.fetchall():
         print(row)
 
-    # Close connection with db
-    cursor.close()
+    cur.close()
     db.close()
