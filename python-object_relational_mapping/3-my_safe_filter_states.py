b1a7a6afb13ab7b97d87e38e5f4ddeca9d9b476c
wwalid18 <agar.io.annonyms@gmail.com>
2025-03-12 11:47:03 +0100

    task 3

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
 """
-Lists all states where the name matches in the database hbtn_0e_0_usa
+Displays all values in the states table of hbtn_0e_0_usa
+where name matches the argument (safe from SQL injection).
 """
 
 import MySQLdb
 import sys
 
+
 if __name__ == "__main__":
-    """main"""
+    """
+    Uses parameterized queries to prevent SQL injection.
+    """
 
     username = sys.argv[1]
     password = sys.argv[2]
