c75e60b2feb3069067fa3c9e3fe8225507595dfb
wwalid18 <agar.io.annonyms@gmail.com>
2025-03-13 10:42:56 +0100

    adjusting

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """
-Prints the first State object from the database hbtn_0e_6_usa.
-If the table is empty, prints "Nothing".
+Prints the first State object from the database
 """
 
 import sys
@@ -9,8 +8,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
