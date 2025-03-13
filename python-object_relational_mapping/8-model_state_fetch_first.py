267b94571953b8ae1be3930df6360e8389c6769d
wwalid18 <agar.io.annonyms@gmail.com>
2025-03-13 10:41:33 +0100

    adjusting

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -11,11 +11,6 @@ from model_state import Base, State
 
 if __name__ == "__main__":
 
-    if len(sys.argv) != 4:
-        print(f"Usage: {sys.argv[0]} "
-              "<mysql username> <mysql password> <database name>")
-        sys.exit(1)
-
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
